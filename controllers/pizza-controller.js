9b1ad8cb418fae11ce52de2adea03ecaa0685cb5
oak45985 <oak45985@gmail.com>
2022-10-02 16:49:49 -0500

    updated, ready to move to lesson 2

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -7,7 +7,7 @@ const pizzaController = {
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
             console.log(err);
-            res.status(400).json(err);
+            res.status(400);
         });
     },
 
